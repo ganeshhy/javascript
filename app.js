9995dee8ba82645832a34fe980ccd7bff62372bf
ganeshhy <ganeshhy1903@gmail.com>
2024-10-20 18:58:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const drumlength=document.querySelectorAll('.drum').length;
+console.log(drumlength);
+for(let i=0;i<drumlength;i++){
+    document.querySelectorAll('drum')[i].addEventListener('click',function(e){
+
+    })
+}
